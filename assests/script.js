cb5ce0d952a5c77f455afd53f6de9ceedb060b6a
Adrienne Kecskes <akecs619@yahoo.com>
2023-05-23 16:03:10 -0500

    initial commit

---
+++ assests/script.js
@@ -0,0 +1,17 @@
+var startButton = document.getElementById("start-button");
+var timerSpan = document.getElementById("time-left");
+
+var timeLeft =  75
+
+function startGame() {
+    var gameInterval = setInterval(() => {
+        timerSpan.textContent = timeLeft --;
+
+    if (!timeLeft) {
+        clearInterval(gameInterval);
+        window.location.href = "highscore.html";
+    } 
+    }, 1000);
+}
+
+startButton.addEventListener("click", startGame);
