823c4a84e79eb411fdef9c19cff885fe73cf64a7
Adrienne Kecskes <akecs619@yahoo.com>
2023-05-24 18:04:33 -0500

    Display correct or wrong text

--- assests/script.js
+++ assests/script.js
@@ -2,47 +2,48 @@ var startButton = document.getElementById("start-button");
 var timerSpan = document.getElementById("time-left");
 var correctAnswer = ""
 var timeLeft = 75
- 
+
 correctAnswer = document.getElementById("answer0").innerHTML
 
 
-function checkAnswer(event){
+function checkAnswer(event) {
     let answer = event.target.innerHTML
 
-if (answer === correctAnswer){
-    console.log("correct")
-
-}
+    if (answer === correctAnswer) {
+        document.getElementById("correctAnswer").style.display = 'block';
+    } else {
+        document.getElementById("wrongAnswer").style.display = 'block';
+     }
 }
 
 function startGame() {
     var question = document.getElementById("question-container")
     question.style.display = "block";
-    
+
     let choices = document.getElementById("question-answers0").children
-    
+
     for (let index = 0; index < choices.length; index++) {
         const element = choices[index];
-        
-    element.addEventListener("click", checkAnswer)
 
-        
+        element.addEventListener("click", checkAnswer)
+
+
     }
 
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     // var gameInterval = setInterval(() => {
     //     timerSpan.textContent = timeLeft--;
 
