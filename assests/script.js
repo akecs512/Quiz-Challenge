29993df825005d905b10e43b270cfa6b800d7110
Adrienne Kecskes <akecs619@yahoo.com>
2023-05-24 17:53:17 -0500

    Check answer correct

--- assests/script.js
+++ assests/script.js
@@ -1,23 +1,58 @@
 var startButton = document.getElementById("start-button");
 var timerSpan = document.getElementById("time-left");
-
+var correctAnswer = ""
 var timeLeft = 75
+ 
+correctAnswer = document.getElementById("answer0").innerHTML
+
+
+function checkAnswer(event){
+    let answer = event.target.innerHTML
+
+if (answer === correctAnswer){
+    console.log("correct")
+
+}
+}
 
 function startGame() {
     var question = document.getElementById("question-container")
     question.style.display = "block";
-    console.log({ question })
+    
+    let choices = document.getElementById("question-answers0").children
+    
+    for (let index = 0; index < choices.length; index++) {
+        const element = choices[index];
+        
+    element.addEventListener("click", checkAnswer)
 
-    var gameInterval = setInterval(() => {
-        timerSpan.textContent = timeLeft--;
+        
+    }
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+    // var gameInterval = setInterval(() => {
+    //     timerSpan.textContent = timeLeft--;
 
 
-        if (!timeLeft) {
-            clearInterval(gameInterval);
-            window.location.href = "highscore.html";
+    //     if (!timeLeft) {
+    //         clearInterval(gameInterval);
+    //         window.location.href = "highscore.html";
 
-        }
-    }, 1000);
+    //     }
+    // }, 1000);
 
 
 }
